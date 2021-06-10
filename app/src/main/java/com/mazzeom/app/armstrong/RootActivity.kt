5b6e43e312c9f2a0c15ee00163451ace486b665a
fine <dlgmltjr0925@gmail.com>
2021-06-11 02:03:48 +0900

    feat: Add back pressed handler for finish

--- app/src/main/java/com/mazzeom/app/armstrong/RootActivity.kt
+++ app/src/main/java/com/mazzeom/app/armstrong/RootActivity.kt
@@ -9,7 +9,20 @@ import com.uber.rib.core.RibActivity
 import com.uber.rib.core.ViewRouter
 
 class RootActivity: RibActivity() {
+    private val FINISH_INTERVAL_TIME = 2000
+    var backPressedTime: Long = 0
+
     override fun createRouter(parentViewGroup: ViewGroup): RootRouter {
         return RootBuilder(object : ParentComponent {}).build(parentViewGroup)
+    }
+
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        val intervalTime = currentTime - backPressedTime
+        if (intervalTime <= FINISH_INTERVAL_TIME) {
+            finish()
+        } else {
+            backPressedTime = currentTime
+        }
     }
 }
